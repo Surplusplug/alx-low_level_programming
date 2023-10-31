973f141be4948e089a43896711646ab7d20dbd16
Surplusplug <joshuaolagidi@gmail.com>
2023-10-31 16:34:54 -0700

    file io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,6 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 {
 	int o, w, len = 0;
+
 	if (filename == NULL)
 		return (-1);
 	if (text_content != NULL)
@@ -22,7 +23,8 @@ int append_text_to_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 			len++;
 	}     o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);if (o == -1 || w == -1)
+	w = write(o, text_content, len);
+	if (o == -1 || w == -1)
 		return (-1);
 	close(o);
 	return (1);
